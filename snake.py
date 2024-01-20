edc5e4d2f46ce046cdd67275adf8743fb13453fe
aaudric <audricgir@gmail.com>
2024-01-20 13:16:34 +0100

    aajoute du code pour le jeu snake

---
+++ snake.py
@@ -0,0 +1,135 @@
+import pygame
+import time
+import random
+
+pygame.init()
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+
+width, height = 600, 400
+game_display = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Snake Game')
+
+clock = pygame.time.Clock()
+snake_block = 10
+snake_speed = 10
+
+font_style = pygame.font.SysFont(None, 50)
+
+def Your_score(score):
+    value = font_style.render("Your Score: " + str(score), True, red)
+    game_display.blit(value, [0, 0])
+
+
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(game_display, green, [x[0], x[1], snake_block, snake_block])
+
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    game_display.blit(mesg, [width / 6, height / 3])
+
+def drawGrid():
+    for x in range(0, width, snake_block):
+        for y in range(0, height, snake_block):
+            rect = pygame.Rect(x, y, snake_block, snake_block)
+            pygame.draw.rect(game_display, white, rect, 1)
+def draw_border():
+    border_color = white  # Vous pouvez choisir n'importe quelle couleur pour la bordure
+    border_width = 2  # Épaisseur de la bordure
+
+    # Dessiner un rectangle autour de l'aire de jeu
+    pygame.draw.rect(game_display, border_color, [0, 0, width, height], border_width)
+
+def gameLoop():
+    score = 0
+    game_over = False
+    game_close = False
+
+    x1 = width / 2
+    y1 = height / 2
+
+    x1_change = 0       
+    y1_change = 0
+
+    snake_List = []
+    Length_of_snake = 1
+
+    foodx = round(random.randrange(0, width - snake_block) / 10.0) * 10.0
+    foody = round(random.randrange(0, height - snake_block) / 10.0) * 10.0
+
+    while not game_over:
+
+        
+        
+        while game_close == True:
+            game_display.fill(white)
+            message("You Lost! Press Q-Quit or C-Play Again", red)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    y1_change = -snake_block
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN:
+                    y1_change = snake_block
+                    x1_change = 0
+
+        if x1 >= width or x1 < 0 or y1 >= height or y1 < 0:
+            game_close = True
+        x1 += x1_change
+        y1 += y1_change
+        gray = (128, 128, 128)
+        game_display.fill(black)  # Changez la couleur du fond ici
+        #drawGrid()  # Dessiner le quadrillage
+        draw_border()        
+        pygame.draw.rect(game_display, red, [foodx, foody, snake_block, snake_block])
+        snake_Head = []
+        snake_Head.append(x1)
+        snake_Head.append(y1)
+        snake_List.append(snake_Head)
+        if len(snake_List) > Length_of_snake:
+            del snake_List[0]
+
+        for x in snake_List[:-1]:
+            if x == snake_Head:
+                game_close = True
+
+        Your_score(score)
+        our_snake(snake_block, snake_List)
+        pygame.display.update()
+
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, width - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, height - snake_block) / 10.0) * 10.0
+            Length_of_snake += 1
+            score += 1 
+
+        pygame.display.update()
+        clock.tick(snake_speed)
+
+    pygame.quit()
+    quit()
+
+gameLoop()
